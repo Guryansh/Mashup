4613ff6f07bac90d4f7315fa9606b4da07f1ba68
Guryansh Singla <108322368+Guryansh@users.noreply.github.com>
2024-08-29 20:50:20 +0530

    Changes

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import os
 os.system("python youtube-downloader.py")
 os.system("python audio-converter.py")
 os.system("python audio-merge.py")
-os.system("python zip.py outputFolder3 outputFolder3.zip")
-os.system("python sendToEmail.py outputFolder3.zip guryanshsingla@gmail.com")
+os.system("python wav-to-mpr3.py")
+os.system("python sendToEmail.py output.mp3 guryanshsingla@gmail.com")
